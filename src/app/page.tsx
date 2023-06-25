174d7ccbcf8111edbec1db11669133042bf17416
Adithya Krishnan <adiadithyakrishnan@gmail.com>
2023-06-25 19:29:08 +0530

    feat : Landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,126 @@
-import SusGoal from "@/components/SusGoal/SusGoal";
+"use client";
 import WorldCount from "@/components/WorldCount/WorldCount";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 export default function Home() {
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const homeRef = useRef<HTMLDivElement>(null);
+  const contactRef = useRef<HTMLDivElement>(null);
+
+  const scrollToAbout = () => {
+    if (aboutRef.current) {
+      aboutRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+  const scrollToHome = () => {
+    if (homeRef.current) {
+      homeRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  const scrollToContact = () => {
+    if (contactRef.current) {
+      contactRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="items-center justify-center m-auto">
-      <WorldCount />
-        
-        
-        
+    <main className="flex min-h-full w-full flex-col items-start justify-between bg-[#F9F2ED] text-black transition-all">
+      <nav className=" flex fixed  w-full h-12 p-10 items-center justify-between">
+        <div className="font-light text-4xl">
+          Sus<span className="font-thin ">Goals*</span>
+        </div>
+        {/*  */}
+        <div className="md:flex hidden gap-5 text-2xl ml-[8%] py-2 px-8 items-center text-center bg-[#f3e6de] rounded-full">
+          <a onClick={scrollToHome}>
+            <div className="font-light hover:cursor-pointer hover:font-bold transition-all duration-300">
+              Home
+            </div>
+          </a>
+          <a onClick={scrollToAbout}>
+            <div className="font-light hover:cursor-pointer hover:font-bold transition-all duration-300">
+              About
+            </div>
+          </a>
+          <div
+            onClick={scrollToContact}
+            className="font-light hover:cursor-pointer hover:font-bold transition-all duration-300"
+          >
+            Contact
+          </div>
+        </div>
+        {/*  */}
+
+        <div
+          onClick={scrollToAbout}
+          className="w-36 h-10 text-center font-light text-xl p-1 border-[1px] border-black rounded-md  hover:shadow-none shadow-[6px_6px_0px_rgb(0,0,0)]  hover:translate-y-0 -translate-y-1 hover:translate-x-0 -translate-x-1 transition-all"
+        >
+          Les Go
+        </div>
+      </nav>
+      <div
+        id="home"
+        ref={homeRef}
+        className="w-full md:h-screen h-full bg-[#F9F2ED]"
+      >
+        <div className="flex flex-col">
+          <div className="md:w-[70%] px-[2%] py-[17%] text-8xl text-ellipsis font-light leading-relaxed transition-all ">
+            Unleash Your Inner <br />{" "}
+            <span className="font-medium">Eco-Ninja</span>, One Dubious
+            Adventure at a Time!
+          </div>
+        </div>
       </div>
+      <div
+        id="about"
+        ref={aboutRef}
+        className="w-full h-screen mt-10 scroll-mt-10  "
+      >
+        <div className="my-20 text bg-black "></div>
+        <WorldCount />
+        <div>
+          <div>What is Sus Goals?</div>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+
+      <footer
+        ref={contactRef}
+        className="w-full min-h-[300px] -mt-5 bg-[#f8e5d8] p-10 justify-end"
+      >
+        <div className="flex justify-between">
+          <div>
+            <div className="m-4 text-6xl text-black font-semibold">
+              Be Sus with us.
+            </div>
+            <div className="m-4 text-3xl text-gray-500">
+             Join today and be part of something great
+            </div>
+            <div className="m-4 text-2xl text-gray-500">
+              Feel free to hit me up with suggestions
+            </div>
+          </div>
+          <div className="mt-10">
+            <div className="text-xl text-black font-semibold">Contact Me</div>
+            <a href="https://github.com/fal3n-4ngel">
+              <div className="text-lg text-gray-500 hover:text-black cursor-pointer transition-all duration-600">
+                Github : fal3n-4ngel
+              </div>
+            </a>
+            <a href="https://www.instagram.com/4di.krish/">
+              <div className="text-lg text-gray-500 hover:text-black cursor-pointer transition-all duration-600">
+                Insta : 4di.krish
+              </div>
+            </a>
+
+            <div className="text-lg text-gray-500 hover:text-black cursor-pointer transition-all duration-600">
+              Reddit : lemme think about it
+            </div>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
--- src/components/WorldCount/WorldCount.tsx
+++ src/components/WorldCount/WorldCount.tsx
@@ -12,134 +12,137 @@ import {
   doc,
 } from "firebase/firestore";
 
+console.log(process.env.FIREBASE_API_KEY);
 
 const firebaseConfig = {
-   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCl0ax0R398LtPDvO1up01uDX0X3vpqPa8",
+  authDomain: "susgoals.firebaseapp.com",
+
+  projectId: "susgoals",
+
+  storageBucket: "susgoals.appspot.com",
+  messagingSenderId: "133809952494",
+  appId: "1:133809952494:web:e5110d8592e752c3a245ff",
   measurementId: "G-EY5GV6ZL1X",
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-
 const sustainableGoals = [
-   "Turn off lights and unplug electronics when not in use",
-   "Take shorter showers to conserve water",
-   "Use reusable water bottles and coffee mugs",
-   "Bring reusable bags for shopping",
-   "Opt for public transportation, biking, or walking instead of driving",
-   "Plant a tree or participate in a local tree-planting initiative",
-   "Sort and recycle household waste properly",
-   "Avoid single-use plastic items like straws, utensils, and food containers",
-   "Use energy-efficient light bulbs",
-   "Repair items instead of replacing them",
-   "Reduce food waste by planning meals and using leftovers",
-   "Choose organic, locally grown produce",
-   "Conserve energy by adjusting thermostat settings and using natural lighting",
-   "Support local farmers and markets by buying locally produced goods",
-   "Reduce paper usage by going digital for bills, receipts, and notes",
-   "Start a compost bin for organic waste",
-   "Volunteer for a community clean-up event",
-   "Turn off the tap while brushing teeth or doing dishes",
-   "Support sustainable and ethical brands when shopping",
-   "Reduce meat consumption and try a vegetarian or vegan meal",
-   "Use eco-friendly cleaning products",
-   "Encourage friends and family to adopt sustainable habits",
-   "Educate yourself about environmental issues and share knowledge",
-   "Donate or recycle old electronics and batteries responsibly",
-   "Conserve water by fixing leaky faucets and pipes",
-   "Choose reusable menstrual products or eco-friendly alternatives",
-   "Opt for natural and homemade beauty and cleaning products",
-   "Practice mindful consumption and avoid unnecessary purchases",
-   "Support renewable energy initiatives and providers",
-   "Join a community garden or start growing your own vegetables",
-   "Reduce shower time and install water-efficient showerheads",
-   "Choose products with minimal packaging or opt for package-free alternatives",
-   "Practice responsible fishing and support sustainable seafood choices",
-   "Avoid idling your vehicle and carpool when possible",
-   "Conserve paper by printing double-sided and using scrap paper for notes",
-   "Reduce plastic waste by using refillable toiletry containers",
-   "Support initiatives to protect and restore natural habitats",
-   "Host a clothing swap with friends or donate unwanted clothing",
-   "Avoid excessive use of air conditioning and heating",
-   "Participate in local sustainability workshops or events",
-   "Install a rain barrel to collect and reuse rainwater",
-   "Choose energy-efficient appliances and electronics",
-   "Use rechargeable batteries instead of disposable ones",
-   "Reduce reliance on disposable products by using cloth napkins and towels",
-   "Explore renewable energy options for your home or community",
-   "Conserve energy by opening windows for natural ventilation",
-   "Avoid using herbicides and pesticides in your garden",
-   "Share sustainable practices and tips on social media",
-   "Practice responsible pet ownership, including proper waste disposal",
-   "Support sustainable tourism practices and eco-friendly accommodations",
-   "Encourage recycling in your workplace or school",
-   "Promote paperless billing and documentation",
-   "Participate in a beach or park clean-up",
-   "Choose eco-friendly and non-toxic household cleaning products",
-   "Reduce plastic waste by using a reusable razor or menstrual cup",
-   "Create a meal plan to reduce food waste and make efficient grocery shopping",
-   "Support renewable energy policies and advocate for sustainable practices",
-   "Choose sustainable and ethically sourced clothing and accessories",
-   "Avoid disposable party supplies and opt for reusable alternatives",
-   "Use natural lighting and adjust blinds to reduce the need for artificial lighting",
-   "Encourage local businesses to adopt sustainable practices",
-   "Properly dispose of hazardous materials like batteries and electronics",
-   "Opt for a digital subscription instead of printed newspapers and magazines",
-   "Start a community recycling program or initiate recycling at work",
-   "Support initiatives to protect endangered species and habitats",
-   "Practice water-saving techniques in your garden, like mulching and drip irrigation",
-   "Reduce food packaging waste by buying in bulk or using refillable containers",
-   "Avoid products with microbeads that harm marine life",
-   "Participate in a clothing repair workshop or learn basic sewing skills",
-   "Choose eco-friendly alternatives for household products, such as bamboo toothbrushes",
-   "Invest in energy-efficient windows and insulation for your home",
-   "Join a local environmental organization or volunteer for environmental causes",
-   "Reduce your consumption of fast fashion and opt for secondhand clothing",
-   "Support renewable energy research and development",
-   "Encourage responsible fishing practices and support sustainable seafood certification",
-   "Minimize food delivery waste by requesting no plastic cutlery or packaging",
-   "Switch to electronic documents for bills, statements, and receipts",
-   "Avoid using disposable wipes and opt for reusable cloth wipes",
-   "Practice sustainable farming techniques like crop rotation and natural pest control",
-   "Reuse glass jars and containers for storage instead of purchasing new ones",
-   "Donate unused or unwanted items to local charities or organizations",
-   "Opt for eco-friendly and sustainable materials in home renovations",
-   "Participate in local initiatives to clean up waterways and protect marine life",
-   "Choose natural and biodegradable materials for personal care products",
-   "Conserve water in your garden by using a drip irrigation system",
-   "Support local initiatives for renewable energy generation",
-   "Avoid printing unnecessary documents and emails",
-   "Participate in a community-led waste reduction campaign",
-   "Opt for energy-saving modes and settings on electronic devices",
-   "Choose sustainably sourced wood products or alternatives to wood",
-   "Support organizations working towards reforestation and forest conservation",
-   "Promote sustainable farming practices and support local farmers",
-   "Reduce light pollution by turning off unnecessary outdoor lighting",
-   "Opt for sustainable and eco-friendly options for home cleaning",
-   "Reduce packaging waste by buying in bulk or choosing products with minimal packaging",
-   "Practice sustainable fishing techniques and follow catch-and-release guidelines",
-   "Choose eco-friendly office supplies like recycled paper and refillable pens",
-   "Educate children and young people about the importance of sustainability",
-   "Support initiatives for renewable energy infrastructure in your community",
-   "Opt for digital invitations and RSVPs instead of paper",
-   "Participate in energy-saving challenges at home or work",
-   "Reduce food waste by composting organic matter",
-   "Choose eco-friendly and sustainable alternatives for personal care products",
-   "Promote responsible water use by fixing leaks and using water-saving devices",
-   "Advocate for and support public transportation improvements in your area",
-   "Avoid using disposable plates and utensils for parties and gatherings",
- ];
- 
- const Index = Math.floor(Math.random() * sustainableGoals.length);
- const String = sustainableGoals[Index];
+  "Turn off lights and unplug electronics when not in use",
+  "Take shorter showers to conserve water",
+  "Use reusable water bottles and coffee mugs",
+  "Bring reusable bags for shopping",
+  "Opt for public transportation, biking, or walking instead of driving",
+  "Plant a tree or participate in a local tree-planting initiative",
+  "Sort and recycle household waste properly",
+  "Avoid single-use plastic items like straws, utensils, and food containers",
+  "Use energy-efficient light bulbs",
+  "Repair items instead of replacing them",
+  "Reduce food waste by planning meals and using leftovers",
+  "Choose organic, locally grown produce",
+  "Conserve energy by adjusting thermostat settings and using natural lighting",
+  "Support local farmers and markets by buying locally produced goods",
+  "Reduce paper usage by going digital for bills, receipts, and notes",
+  "Start a compost bin for organic waste",
+  "Volunteer for a community clean-up event",
+  "Turn off the tap while brushing teeth or doing dishes",
+  "Support sustainable and ethical brands when shopping",
+  "Reduce meat consumption and try a vegetarian or vegan meal",
+  "Use eco-friendly cleaning products",
+  "Encourage friends and family to adopt sustainable habits",
+  "Educate yourself about environmental issues and share knowledge",
+  "Donate or recycle old electronics and batteries responsibly",
+  "Conserve water by fixing leaky faucets and pipes",
+  "Choose reusable menstrual products or eco-friendly alternatives",
+  "Opt for natural and homemade beauty and cleaning products",
+  "Practice mindful consumption and avoid unnecessary purchases",
+  "Support renewable energy initiatives and providers",
+  "Join a community garden or start growing your own vegetables",
+  "Reduce shower time and install water-efficient showerheads",
+  "Choose products with minimal packaging or opt for package-free alternatives",
+  "Practice responsible fishing and support sustainable seafood choices",
+  "Avoid idling your vehicle and carpool when possible",
+  "Conserve paper by printing double-sided and using scrap paper for notes",
+  "Reduce plastic waste by using refillable toiletry containers",
+  "Support initiatives to protect and restore natural habitats",
+  "Host a clothing swap with friends or donate unwanted clothing",
+  "Avoid excessive use of air conditioning and heating",
+  "Participate in local sustainability workshops or events",
+  "Install a rain barrel to collect and reuse rainwater",
+  "Choose energy-efficient appliances and electronics",
+  "Use rechargeable batteries instead of disposable ones",
+  "Reduce reliance on disposable products by using cloth napkins and towels",
+  "Explore renewable energy options for your home or community",
+  "Conserve energy by opening windows for natural ventilation",
+  "Avoid using herbicides and pesticides in your garden",
+  "Share sustainable practices and tips on social media",
+  "Practice responsible pet ownership, including proper waste disposal",
+  "Support sustainable tourism practices and eco-friendly accommodations",
+  "Encourage recycling in your workplace or school",
+  "Promote paperless billing and documentation",
+  "Participate in a beach or park clean-up",
+  "Choose eco-friendly and non-toxic household cleaning products",
+  "Reduce plastic waste by using a reusable razor or menstrual cup",
+  "Create a meal plan to reduce food waste and make efficient grocery shopping",
+  "Support renewable energy policies and advocate for sustainable practices",
+  "Choose sustainable and ethically sourced clothing and accessories",
+  "Avoid disposable party supplies and opt for reusable alternatives",
+  "Use natural lighting and adjust blinds to reduce the need for artificial lighting",
+  "Encourage local businesses to adopt sustainable practices",
+  "Properly dispose of hazardous materials like batteries and electronics",
+  "Opt for a digital subscription instead of printed newspapers and magazines",
+  "Start a community recycling program or initiate recycling at work",
+  "Support initiatives to protect endangered species and habitats",
+  "Practice water-saving techniques in your garden, like mulching and drip irrigation",
+  "Reduce food packaging waste by buying in bulk or using refillable containers",
+  "Avoid products with microbeads that harm marine life",
+  "Participate in a clothing repair workshop or learn basic sewing skills",
+  "Choose eco-friendly alternatives for household products, such as bamboo toothbrushes",
+  "Invest in energy-efficient windows and insulation for your home",
+  "Join a local environmental organization or volunteer for environmental causes",
+  "Reduce your consumption of fast fashion and opt for secondhand clothing",
+  "Support renewable energy research and development",
+  "Encourage responsible fishing practices and support sustainable seafood certification",
+  "Minimize food delivery waste by requesting no plastic cutlery or packaging",
+  "Switch to electronic documents for bills, statements, and receipts",
+  "Avoid using disposable wipes and opt for reusable cloth wipes",
+  "Practice sustainable farming techniques like crop rotation and natural pest control",
+  "Reuse glass jars and containers for storage instead of purchasing new ones",
+  "Donate unused or unwanted items to local charities or organizations",
+  "Opt for eco-friendly and sustainable materials in home renovations",
+  "Participate in local initiatives to clean up waterways and protect marine life",
+  "Choose natural and biodegradable materials for personal care products",
+  "Conserve water in your garden by using a drip irrigation system",
+  "Support local initiatives for renewable energy generation",
+  "Avoid printing unnecessary documents and emails",
+  "Participate in a community-led waste reduction campaign",
+  "Opt for energy-saving modes and settings on electronic devices",
+  "Choose sustainably sourced wood products or alternatives to wood",
+  "Support organizations working towards reforestation and forest conservation",
+  "Promote sustainable farming practices and support local farmers",
+  "Reduce light pollution by turning off unnecessary outdoor lighting",
+  "Opt for sustainable and eco-friendly options for home cleaning",
+  "Reduce packaging waste by buying in bulk or choosing products with minimal packaging",
+  "Practice sustainable fishing techniques and follow catch-and-release guidelines",
+  "Choose eco-friendly office supplies like recycled paper and refillable pens",
+  "Educate children and young people about the importance of sustainability",
+  "Support initiatives for renewable energy infrastructure in your community",
+  "Opt for digital invitations and RSVPs instead of paper",
+  "Participate in energy-saving challenges at home or work",
+  "Reduce food waste by composting organic matter",
+  "Choose eco-friendly and sustainable alternatives for personal care products",
+  "Promote responsible water use by fixing leaks and using water-saving devices",
+  "Advocate for and support public transportation improvements in your area",
+  "Avoid using disposable plates and utensils for parties and gatherings",
+];
+
+const Index = Math.floor(Math.random() * sustainableGoals.length);
+const String = sustainableGoals[Index];
 
 let val1 = 0;
+let done = false;
 
 export const db = getFirestore(app);
 // Initialize Firebase
@@ -156,34 +159,84 @@ function WorldCount() {
     setTimeout(() => {
       val1 = val;
       setCount(val);
-    }, 1000);
+    }, 100);
   };
 
+  const increment = async () => {
+    const numberDoc = doc(db, "count", "count");
+    done = true;
+    await updateDoc(numberDoc, {
+      val: countValue + 1,
+    });
+  };
   useEffect(() => {
     const fetchData = async () => {
       getValue();
     };
-    fetchData(); // Fetch initial data
+    fetchData();
 
-    const interval = setInterval(fetchData, 5000); // Fetch data every 5 seconds
+    const interval = setInterval(fetchData, 2000);
 
     return () => {
-      clearInterval(interval); // Clean up the interval on component unmount
+      clearInterval(interval);
     };
   });
 
   return (
     <div>
-      <div className="text-8xl text-center">{countValue}</div>
-
-      <div className="items-center justify-center m-auto text-4xl text-center">
-        {String}
-      </div>
-
-      <div className="p-10 w-[75%] mx-auto flex justify-between">
-          <div className="bg-transparent h-[10%] w-[30%] text-center hover:bg-white font-semibold hover:text-black py-2 px-4 border border-white hover:border-slate-700 rounded">DONE</div>
-          <div className="bg-transparent h-[10%] w-[30%] text-center hover:bg-white font-semibold hover:text-black py-2 px-4 border border-white hover:border-slate-700 rounded">NOT YET</div>
+      <div id="counter" className="w-full">
+        <div className="flex md:flex-row flex-col w-[95%]  m-auto justify-between">
+          <div className="min-w-[50%] h-[50%] m-auto md:border-r-2 border-black ">
+            <div className="text-6xl text-center font-light my-10">
+              ToDaYs TaSk
+            </div>
+            <div className="w-[90%] mt-10  text-5xl font-light text-center">
+              &ldquo;{String}&rdquo;
+            </div>
+            <div className="flex justify-center mt-10">
+              {!done && (
+                <button
+                  className="bg-black text-white rounded-full w-28 h-28 flex items-center justify-center"
+                  onClick={increment}
+                >
+                  <svg
+                    className="w-16 h-16 text-green-100"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M5 13l4 4L19 7"
+                    ></path>
+                  </svg>
+                </button>
+              )}
+            </div>
+          </div>
+          <div className=" w-[50%]  items-center mx-auto">
+            <div className="text-[150px] text-center  mx-auto mt-10">
+              {" "}
+              {countValue}
+            </div>
+            {!done && (
+              <div className=" text-center text-4xl   mx-auto font-light p-5">
+                peeps are #sustainablegoals!. Wanna be{" "}
+                <span className="font-light">{countValue + 1}</span> th peep to
+                be sus.
+              </div>
+            )}
+            {done && (
+              <div className=" text-center text-4xl   mx-auto font-light p-5">
+                peeps are #sustainablegoals!. Thanks for bein a part of this.
+              </div>
+            )}
+          </div>
         </div>
+      </div>
     </div>
   );
 }
